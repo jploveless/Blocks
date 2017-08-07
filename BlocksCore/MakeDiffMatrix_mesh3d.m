c626b48305f942d2bfe0435c5b1f36a144470768
Jack Loveless <jloveles@smith.edu>
2017-08-07 12:55:42 -0400

    Updated diff matrix construction

--- BlocksCore/MakeDiffMatrix_mesh3d.m
+++ BlocksCore/MakeDiffMatrix_mesh3d.m
@@ -7,42 +7,33 @@ function Diff = MakeDiffMatrix_mesh3d(Patches)
 %   Based on Eileen Evans' MakeDiffMatrix_mesh2d.
 %
 
+snel = sum(Patches.nEl);
+
 % Determine adjacency matrix
-share = SideShare(Patches.v)';
-share = repmat(share, 9, 1);
-keep = share > 0;
-share = share(keep);
+share = SideShare(Patches.v)'; 
+share(share == 0) = -1e20;
+share = sort(share);
+share(share < repmat(1:snel, 3, 1)) = 0;
+Share = zeros(9, snel);
+Share(1:3:end, :) = 3*share-2;
+Share(2:3:end, :) = 3*share-1;
+Share(3:3:end, :) = 3*share;
+keep = Share > 0;
+share = Share(keep);
 
 % Row indices. Same size as share, for linear indexing
-rows = repmat(1:sum(Patches.nEl), 9, 1); 
-rows = rows(keep);
+rows = 1:9*snel;
+cols = repmat(reshape(1:3*snel, 3, snel), 3, 1);
+cols = cols(keep);
+rows = rows(keep)';
 
 % Convert row indices and share column indices to linear indices
-szel = [9*sum(Patches.nEl), 3*sum(Patches.nEl)];
-% Strike slip share
-linind1 = sub2ind(szel, 9*rows-8, 3*share-2);
-linind2 = sub2ind(szel, 9*rows-7, 3*share-2);
-linind3 = sub2ind(szel, 9*rows-6, 3*share-2);
-% Dip slip share
-linind4 = sub2ind(szel, 9*rows-5, 3*share-1);
-linind5 = sub2ind(szel, 9*rows-4, 3*share-1);
-linind6 = sub2ind(szel, 9*rows-3, 3*share-1);
-% Tensile slip share
-linind7 = sub2ind(szel, 9*rows-2, 3*share-0);
-linind8 = sub2ind(szel, 9*rows-1, 3*share-0);
-linind9 = sub2ind(szel, 9*rows-0, 3*share-0);
+szel = [9*snel, 3*snel]; % 9 rows for each element = 3 neighbors*3 slip components
+adjaind = sub2ind(szel, rows, share);
+selfind = sub2ind(szel, rows, cols);
 
 % Create square difference array
 Diff = zeros(szel);
-Diff(1:3:end, :) = eye(3*sum(Patches.nEl)); % Positive one on the main diagonal
-Diff(2:3:end, :) = eye(3*sum(Patches.nEl)); % Positive one on the main diagonal
-Diff(3:3:end, :) = eye(3*sum(Patches.nEl)); % Positive one on the main diagonal
-Diff(linind1) = -1; % Negative one in columns corresponding to adjacent elements
-Diff(linind2) = -1;
-Diff(linind3) = -1;
-Diff(linind4) = -1;
-Diff(linind5) = -1;
-Diff(linind6) = -1;
-Diff(linind7) = -1;
-Diff(linind8) = -1;
-Diff(linind9) = -1;
+Diff(selfind) = 1; % Positive one in columns corresponding to particular elements
+Diff(adjaind) = -1; % Negative one in columns corresponding to adjacent elements
+Diff = Diff(keep, :);
